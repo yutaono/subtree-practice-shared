9d35ee3d28f29d61db30d7da2ba10f4622256a5e
yutaono <vrf.ced@gmail.com>
2015-11-16 17:27:37 +0900

    add isPrime method

--- util/src/main/scala/practice/util/Utility.scala
+++ util/src/main/scala/practice/util/Utility.scala
@@ -12,6 +12,11 @@ object Utility {
     def `++`: Int = value + 1
     def `--`: Int = value - 1
     def `**`(x: Double): Double = Math.pow(value, x)
+    def isPrime: Boolean = primes.contains(value)
+    lazy val primes: Set[Int] = {
+       val r = (2 to value).toSet
+       r.foldLeft(r)((ps, x) => if (ps(x)) ps -- (x * x to value by x) else ps)
+    }
   }
   
 }
