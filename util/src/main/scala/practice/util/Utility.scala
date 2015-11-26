f8c8af3efad0c3f2bd8911793f5a21ea04842a16
Emberton Jonathan <jaemberton@gmail.com>
2015-11-26 10:44:01 +0900

    Added isOddNumber function to Utility.scala

--- util/src/main/scala/practice/util/Utility.scala
+++ util/src/main/scala/practice/util/Utility.scala
@@ -13,6 +13,7 @@ object Utility {
     def `--`: Int = value - 1
     def `**`(x: Double): Double = Math.pow(value, x)
     def isPrime: Boolean = primes.contains(value)
+    def isOddNumber: Boolean = { value % 2 != 0 }
     lazy val primes: Set[Int] = {
       val r = (2 to value).toSet
       r.foldLeft(r)((ps, x) => if (ps(x)) ps -- (x * x to value by x) else ps)
@@ -20,5 +21,5 @@ object Utility {
     def isPerfectNumber: Boolean = value == divisors.sum
     lazy val divisors: List[Int] = (value / 2 to 1 by -1).foldLeft[List[Int]](Nil)((ns, n) => if (value % n == 0) n :: ns else ns)
   }
-  
+
 }
